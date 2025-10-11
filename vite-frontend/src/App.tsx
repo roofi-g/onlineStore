35f605e173addf982b2ddc4c3ee9ad54f63ca3f9
Roofi <roofi.g@yandex.ru>
2025-10-11 12:13:01 +0500

    feat: configured route. moved route to a separate file

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
-import Header from './components/Header.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {ROUTES} from "./routes";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Favorite from "./pages/Favorite";
+import Account from "./pages/Account";
+import ProductsList from "./pages/ProductsList";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route exact path={ROUTES.home} element={<Home />}/>
+            <Route exact path={ROUTES.cart} element={<Cart />}/>
+            <Route exact path={ROUTES.favorite} element={<Favorite />}/>
+            <Route exact path={ROUTES.account} element={<Account />}/>
+            <Route exact path={ROUTES.products} element={<ProductsList />}/>
+            <Route exact path="/products/:id" element={<ProductDetail />}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ vite-frontend/src/routes.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+    home: "/",
+    cart: "/cart",
+    favorite: "/favorite",
+    account: "/account",
+    products: "/products",
+    productDetail: (id) => `/products/${id}`,
+}
