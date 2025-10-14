2d2e4e97e039a65cd8b64000e8b666952374ae7e
Roofi <roofi.g@yandex.ru>
2025-10-15 01:16:29 +0500

    feat: changed static menu to dynamic menu

--- vite-frontend/src/components/header/DropdownMenu.tsx
+++ vite-frontend/src/components/header/DropdownMenu.tsx
@@ -1,17 +1,34 @@
+import { useGetCategoriesQuery } from "../../features/products/productsApi";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../routes";
+import { useState } from "react";
+
 export default function DropdownMenu() {
+    const { data: categories = [] } = useGetCategoriesQuery();
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
-        <div className="absolute top-full shadow">
-            <nav>
-                <div>
-                    <a href="">Одежда</a>
-                </div>
-                <div>
-                    <a href="">Аксессуары</a>
-                </div>
-                <div>
-                    <a href="">Обувь</a>
-                </div>
-            </nav>
-        </div>
+        <section className="absolute top-full bg-white z-50 w-100 p-4">
+            <div className="grid grid-cols-2">
+                <ul className="col-start-1 text-lg font-semibold tracking-normal leading-8">
+                    {categories.map(categorise =>
+                        <li key={categorise.id} className="hover:text-rose-400" onMouseEnter={() => setShowMenu(categorise.id)}>
+                            {categorise.parentId === null && (
+                                <Link to={ROUTES.categories(categorise.id)}>{categorise.name}</Link>
+                            )}
+                        </li>
+                    )}
+                </ul>
+                <ul className="col-start-2">
+                    {categories.map(categorise =>
+                        <li key={categorise.id} className="hover:text-rose-400">
+                            {categorise.parentId === showMenu && (
+                                <Link to={ROUTES.categories(categorise.id)}>{categorise.name}</Link>
+                            )}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </section>
     )
 }
