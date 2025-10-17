690d2b52418f1e0cc5f56f228d6af88b31c081f7
Roofi <roofi.g@yandex.ru>
2025-10-17 22:40:58 +0500

    feat: functionality for adding the slug property

--- vite-frontend/src/features/products/productsApi.tsx
+++ vite-frontend/src/features/products/productsApi.tsx
@@ -1,14 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import slugify from "slugify";
 
 export const productsApi = createApi({
     reducerPath: 'productsApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3005/' }),
+    tagTypes: ['Products', 'Catalog'],
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: () => 'products',
+            providesTags: ['Products'],
         }),
         getCatalog: builder.query({
             query: () => 'catalog',
+            providesTags: ['Catalog'],
+            transformResponse: (response) =>
+                response.map((catalog) => ({
+                    ...catalog,
+                    slug: slugify(catalog.name, { lower: true, strict: true, locale: "ru" }),
+                })),
         }),
     }),
 });
