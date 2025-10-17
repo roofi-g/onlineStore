a5ca555fdabfce05fc2cf90c0cadfcc569303079
Roofi <roofi.g@yandex.ru>
2025-10-17 22:52:52 +0500

    refactor: moved all routes to a separate file. configured paths

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,31 +1,16 @@
 import './App.css'
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {ROUTES} from "./routes";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Favorite from "./pages/Favorite";
-import Account from "./pages/Account";
-import ProductDetail from "./pages/ProductDetail";
-import CatalogPage from "./pages/CatalogPage";
+import { useRoutes } from "react-router-dom";
+import { routes } from "./routes/routes";
 
 function App() {
-  return (
+    const routing = useRoutes(routes);
+    return (
       <Provider store={store}>
-          <BrowserRouter>
-              <Routes>
-                  <Route exact path={ROUTES.home} element={<Home />}/>
-                  <Route exact path={ROUTES.cart} element={<Cart />}/>
-                  <Route exact path={ROUTES.favorite} element={<Favorite />}/>
-                  <Route exact path={ROUTES.account} element={<Account />}/>
-                  {/*<Route exact path={ROUTES.products} element={<CategoryPage />}/>*/}
-                  <Route exact path="/products/:id" element={<ProductDetail />}/>
-                  <Route exact path="/catalog/:id" element={<CatalogPage />}/>
-              </Routes>
-          </BrowserRouter>
+          { routing }
       </Provider>
-  )
+    )
 }
 
 export default App
--- vite-frontend/src/routes/routes.tsx
+++ vite-frontend/src/routes/routes.tsx
@@ -1,9 +1,33 @@
-export const ROUTES = {
-    home: "/",
-    cart: "/cart",
-    wishlists: "/wishlists",
-    account: "/account",
-    products: "/products",
-    productDetail: (id) => `/products/${id}`,
-    catalog: (category) => `/catalog/${category}`,
-}
+import MainLayout from "../components/MainLayout";
+import HomePage from "../pages/HomePage";
+import CartPage from "../pages/CartPage";
+import WishListsPage from "../pages/WishListsPage";
+import AccountPage from "../pages/AccountPage";
+import CatalogLayout from "../components/catalog/CatalogLayout";
+import CatalogPage from "../pages/CatalogPage";
+import ProductPage from "../pages/ProductPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+export const routes = [
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: 'cart', element: <CartPage /> },
+            { path: 'wishlists', element: <WishListsPage /> },
+            { path: 'account', element: <AccountPage /> },
+            {
+                path: 'catalog',
+                element: <CatalogLayout />,
+                children: [
+                    { index: true, element: <CatalogPage /> },
+                    { path: ':categorySlug', element: <CatalogPage /> },
+                    { path: ':categorySlug/:subCategorySlug', element: <CatalogPage /> },
+                    { path: ':categorySlug/:subCategorySlug/:productId', element: <ProductPage /> },
+                ]
+            }
+        ],
+    },
+    { path: '*', element: <NotFoundPage /> },
+];
