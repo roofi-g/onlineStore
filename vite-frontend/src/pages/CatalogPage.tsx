6846c13f8de6aea3725394d5f97164520dc08571
Roofi <roofi.g@yandex.ru>
2025-10-20 13:46:05 +0500

    feat: took out two hooks - useActiveCategory, useCategoryFilter

--- vite-frontend/src/pages/CatalogPage.tsx
+++ vite-frontend/src/pages/CatalogPage.tsx
@@ -1,35 +1,22 @@
-import {Link, useParams} from "react-router-dom";
-import {useGetCatalogQuery, useGetProductsQuery} from "../features/products/productsApi";
-import {useMemo, useState} from "react";
-import BreadCrumbs from "../components/catalog/BreadCrumbs";
+import { useGetProductsQuery } from "../features/products/productsApi";
+import { useCategoryFilter } from "../features/catalog/useCategoryFilter";
+import { useActiveCategory } from "../features/catalog/useActiveCategory";
 
 export default function CatalogPage() {
-    const { categorySlug, subCategorySlug } = useParams();
-    const { data: catalog = [] } = useGetCatalogQuery();
+    const { activeCategory } = useActiveCategory();
     const { data: products = [] } = useGetProductsQuery();
 
-    const parentCategory = catalog.find(cat => cat.slug === categorySlug);
-    const childCategory = catalog.find(cat => cat.slug === subCategorySlug && cat.parentId === parentCategory?.id);
-    const activeProducts = childCategory || parentCategory;
-
-    const filteredProducts = useMemo(() => {
-        if (!activeProducts) return [];
-        return products.filter(p =>
-            Array.isArray(p.category_id)
-                ? p.category_id.includes(activeProducts.id)
-                : p.category_id === activeProducts.id
-        )
-    }, [products, activeProducts])
+    const categoryFilter = useCategoryFilter(products, activeCategory);
 
     return (
         <div>
-            <BreadCrumbs />
             <div className="flex flex-wrap justify-around">
-                {filteredProducts
+                {categoryFilter
                     .map(el =>
                         <div key={el.id}>
                             <img className="w-70" src={el.image} alt=""/>
                             <p>{el.name}</p>
+                            <p>{el.price}</p>
                         </div>
                     )
                 }
