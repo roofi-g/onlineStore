5f89cb4c4309fc8ceebf13b3b50ca84a92a82ed1
Roofi <roofi.g@yandex.ru>
2025-11-22 01:58:18 +0500

    refactor + feat: changed the global store from toolkit to RTK, basic product card display

--- vite-frontend/src/features/products/cartApi.tsx
+++ vite-frontend/src/features/products/cartApi.tsx
@@ -1,17 +1,32 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [],
-  },
-  reducers: {
-    addToCart: (state, action) => {
-      const newArr = [...state.items, action.payload];
-      return {...state, items: newArr};
-    }
-  }
-})
+export const cartApi = createApi({
+  reducerPath: 'cartApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3005/' }),
+  tagTypes: ['Cart'],
+  endpoints: (builder) => ({
+    getCart: builder.query({
+      query: () => 'cart',
+      providesTags: ['Cart'],
+    }),
+    addToCart: builder.mutation({
+      query: (product) => ({
+        url: 'cart',
+        method: 'POST',
+        body: { ...product, quantity: 1 },
+        // body: { productId: product.id, size: product.sizes, quantity: 1 },
+      }),
+      invalidatesTags: ['Cart'],
+    }),
+    // removeToCart: builder.mutation({
+    //   query: (product) => ({
+    //     url: 'cart',
+    //     method: 'DELETE',
+    //     body: { ...product, quantity: 1 },
+    //   }),
+    //   invalidatesTags: ['Cart'],
+    // }),
+  }),
+});
 
-export const { addToCart } = cartSlice.actions;
-export default cartSlice.reducer;
+export const { useGetCartQuery, useAddToCartMutation, useRemoveToCartMutation } = cartApi;
