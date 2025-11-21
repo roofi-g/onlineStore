2955a2cef07362cdbdd04cacbe2df48a3c26c86c
Roofi <roofi.g@yandex.ru>
2025-11-22 02:19:32 +0500

    refactor: changes to DropdownMenu

--- vite-frontend/src/components/header/DropdownMenu.tsx
+++ vite-frontend/src/components/header/DropdownMenu.tsx
@@ -1,21 +1,31 @@
 import { useGetCatalogQuery } from "../../features/products/productsApi";
+import { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import type { Catalog } from '@/types';
 
 export default function DropdownMenu() {
-    const { data: catalog = [] } = useGetCatalogQuery();
-    const [selectedParent, setSelectedParent] = useState('');
+    const { data: catalog = [], isLoading } = useGetCatalogQuery();
+    const [selectedParent, setSelectedParent] = useState<number | null>(null);
 
-    // function buildCategoryTree(catalog) {
-    //     return catalog
-    //         .filter(category => !category.parent)
-    //         .map(parent => ({
-    //             ...parent,
-    //             children: catalog.filter(sub => sub.parentId === parent.id)
-    //         }))
-    // }
+    const parents = useMemo(
+        () => catalog.filter((cat: Catalog) => !cat.parentId),
+        [catalog]
+    );
 
-    const parents = catalog.filter(category => !category.parent);
+    const parentMap = useMemo(() => {
+        const map = new Map<number, Catalog>();
+        parents.forEach((p) => map.set(p.id, p))
+        return map;
+    }, [parents]);
+
+    const children = useMemo(() => {
+        if (!selectedParent) return [];
+        return catalog.filter(
+            (sub: Catalog) => Number(sub.parentId) === Number(selectedParent)
+        );
+    }, [catalog, selectedParent]);
+
+    if (isLoading) return <p>Loading...</p>;
 
     return (
         <section className="absolute top-full bg-white z-50 w-100 p-4">
@@ -23,23 +33,25 @@ export default function DropdownMenu() {
                 <ul className="col-start-1 text-lg font-semibold tracking-normal leading-8">
                     {parents.map(catalog =>
                         <li key={catalog.id} className="hover:text-rose-400" >
-                            {catalog.parentId === null && (
-                                <Link onMouseEnter={() => setSelectedParent(catalog.id)} to={`/catalog/${catalog.slug}`}>{catalog.name}</Link>
-                            )}
+                            <Link
+                                onMouseEnter={() => setSelectedParent(catalog.id)}
+                                to={`/catalog/${catalog.slug}`}>{catalog.name}
+                            </Link>
                         </li>
                     )}
                 </ul>
                 <ul className="col-start-2">
-                    {catalog
-                        .filter(sub => sub.parentId === selectedParent)
-                        .map(sub => {
-                            const parent = parents.find(p => p.id === sub.parentId);
+                    {children.length > 0 && (
+                        children.map(sub => {
+                            const parent = parentMap.get(sub.parentId!);
+                            // console.log(parent)
+                            if (!parent || !parent.slug || !sub.slug) return null;
                             return (
                                 <li key={sub.id} className="hover:text-rose-400">
                                     <Link to={`/catalog/${parent.slug}/${sub.slug}`}>{sub.name}</Link>
                                 </li>
-                            )
-                        }
+                            );
+                        })
                     )}
                 </ul>
             </div>
