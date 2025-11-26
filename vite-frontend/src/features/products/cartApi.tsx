64184af04b8f67a63c077614b9bf33702cf6e04b
Roofi <roofi.g@yandex.ru>
2025-11-26 16:01:34 +0500

    feat: deleting an item in the shopping cart via RTK

--- vite-frontend/src/features/products/cartApi.tsx
+++ vite-frontend/src/features/products/cartApi.tsx
@@ -13,19 +13,26 @@ export const cartApi = createApi({
       query: (product) => ({
         url: 'cart',
         method: 'POST',
-        body: { ...product, quantity: 1 },
-        // body: { productId: product.id, size: product.sizes, quantity: 1 },
+        // body: { ...product, quantity: 1 },
+        body: {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          image: product.image,
+          size: product.size,
+          discount: product.discount,
+          quantity: 1
+        },
       }),
       invalidatesTags: ['Cart'],
     }),
-    // removeToCart: builder.mutation({
-    //   query: (product) => ({
-    //     url: 'cart',
-    //     method: 'DELETE',
-    //     body: { ...product, quantity: 1 },
-    //   }),
-    //   invalidatesTags: ['Cart'],
-    // }),
+    removeToCart: builder.mutation({
+      query: (id) => ({
+        url: `cart/${id}`,
+        method: 'DELETE'
+      }),
+      invalidatesTags: ['Cart'],
+    }),
   }),
 });
 
