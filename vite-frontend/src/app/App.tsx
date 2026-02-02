693af82e6f294413d1f99a7c5da7857e776c7fb0
Roofi <roofi.g@yandex.ru>
2026-02-02 14:09:54 +0500

    refactor: moved the root files to the app

---
+++ vite-frontend/src/app/App.tsx
@@ -0,0 +1,16 @@
+import './providers/theme.css'
+import { Provider } from "react-redux";
+import { store } from "./providers/store";
+import { useRoutes } from "react-router-dom";
+import { routes } from "./providers/routes";
+
+function App() {
+  const routing = useRoutes(routes);
+  return (
+      <Provider store={store}>
+        { routing }
+      </Provider>
+  )
+}
+
+export default App
