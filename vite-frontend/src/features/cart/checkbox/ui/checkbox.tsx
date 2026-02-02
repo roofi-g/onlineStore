42e3b1eee584cd15d35655b7b766888c8a60917d
Roofi <roofi.g@yandex.ru>
2026-02-02 21:24:22 +0500

    feat: features with minimal data

---
+++ vite-frontend/src/features/cart/checkbox/ui/checkbox.tsx
@@ -0,0 +1,11 @@
+export const Сheckbox = () => {
+  return (
+    <>
+      <input className="mx-4 p-2 w-3 h-3
+          appearance-none border border-zinc-400 rounded
+          checked:[background-image:url('./assets/icons/check.svg')]
+          bg-size-[10px] bg-center bg-no-repeat" type="checkbox"
+      />
+    </>
+  )
+}
