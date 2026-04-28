7284e6b1560d41163f538b56fc9918d9482d0da1
Roofi <roofi.g@yandex.ru>
2026-04-28 12:55:34 +0500

    refactor: move sorting and filtering logic into separate components. I also connected a selector to display the number of products.

--- vite-frontend/src/pages/catalog/CatalogPage.tsx
+++ vite-frontend/src/pages/catalog/CatalogPage.tsx
@@ -1,85 +1,28 @@
-import { useEffect, useMemo, useState } from "react";
-import Filter from "../../widgets/catalog/ui/Filter";
-import Sort from "../../features/catalog/sort/ui/Sort";
-import { useProductsByCategory } from "../../entities/catalog/model/useProductsByCategory";
-import { useFilterProducts } from "../../features/catalog/filter/model/useFilterProducts";
-import { sortProducts } from "../../features/catalog/sort/model/useSortProducts";
-import ProductsList from "../../entities/product/ui/ProductsList";
-import { getMinMaxPrice } from "../../shared/model/getMinMaxPrice";
-import { getUniqueSizes } from "../../shared/model/getUniqueSizes";
+import Filter from '../../features/catalog/filter/ui/Filter';
+import Search from '../../features/catalog/search/ui/Search';
+import Sort from '../../features/catalog/sort/ui/Sort';
+import ProductsList from '../../entities/product/ui/ProductsList';
+import Pagination from '../../features/catalog/pagination/ui/Pagination';
+import { useSelector } from 'react-redux';
+import { useActiveCategory } from '../../entities/catalog/model/useActiveCategory';
+import { selectFilteredCount } from '../../features/catalog/filter/model/selectors';
 
 export default function CatalogPage() {
-	const products = useProductsByCategory();
+  const { activeCategory } = useActiveCategory();
+  const productsCount = useSelector((state) => selectFilteredCount(state, activeCategory));
 
-	const [draftFilters, setDraftFilters] = useState({
-		sizes: [],
-		price: null as [number, number] | null
-	})
-
-	const [appliedFilters, setAppliedFilters] = useState({
-		sizes: [],
-		price: null as [number, number] | null
-	})
-
-	const [sort, setSort] = useState('hot');
-
-	const { minPrice, maxPrice } = useMemo(
-		() => getMinMaxPrice(products),
-		[products]
-	);
-
-	const sizes = useMemo(
-		() => getUniqueSizes(products),
-		[products]
-	);  
-
-	useEffect(() => {
-		if (minPrice && maxPrice) {
-
-			const range: [number, number] = [minPrice, maxPrice]
-
-			setDraftFilters({
-				sizes: [],
-				price: range
-			})
-
-			setAppliedFilters({
-				sizes: [],
-				price: range
-			})
-		}
-
-	}, [minPrice, maxPrice])
-
-	const filteredProducts = useFilterProducts(products, appliedFilters);
-
-	const displayed = useMemo(
-		() => sortProducts(filteredProducts, sort),
-		[filteredProducts, sort]
-	);
-
-	const handleApplyFilters = () => {
-		setAppliedFilters(draftFilters)
-	}
-
-	return (
-		<div>
-			<div className="flex justify-between mt-5 mb-5">
-				<Filter
-					filters={draftFilters}
-					setFilters={setDraftFilters}
-					sizes={sizes}
-					minPrice={minPrice}
-					maxPrice={maxPrice}
-					productsLength={filteredProducts.length}
-					onApply={handleApplyFilters}
-				/>
-				<p>{displayed.length} товаров</p>
-				<Sort
-					appliedSort={sort}
-					handleApplySort={setSort} />
-			</div>
-			<ProductsList displayed={displayed} />
-		</div>
-	)
-}
+  return (
+    <div>
+      <div className="flex justify-between items-center mt-5 mb-5">
+        <Filter />
+        <p>{productsCount} товарa/ов</p>
+        <div className="flex gap-2">
+          {/* <Search /> */}
+          <Sort />
+        </div>
+      </div>
+      <ProductsList />
+      <Pagination />
+    </div>
+  );
+};
